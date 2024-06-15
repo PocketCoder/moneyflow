6a6a7aa13c898166979cf7a306d867b1d83a1654
PocketCoder <jake.williams77@gmail.com>
2024-06-15 16:38:23 +0100

    feat: Amended divider

--- frontend/src/components/accounts/BankRows.tsx
+++ frontend/src/components/accounts/BankRows.tsx
@@ -1,25 +1,14 @@
 import AccountCard from './AccountCard';
-import Divider from './Divider';
+import {Divider} from '@tremor/react';
 
 const BankRows = ({banks, accounts, openModal}) => {
 	const sortedBanks = banks.sort((a, b) => a.localeCompare(b));
-	const bankTotals = {};
-
-	for (const account of accounts) {
-		const bank = account.parent;
-		bankTotals[bank] === undefined
-			? (bankTotals[bank] = Number(account.balance))
-			: (bankTotals[bank] += Number(account.balance));
-	}
-
-	const formatAmount = (num: number) =>
-		new Intl.NumberFormat('en-GB', {minimumSignificantDigits: 2, maximumSignificantDigits: 2}).format(num).toString();
 
 	return (
 		<>
 			{sortedBanks.map((bank, index) => (
 				<div key={index} className="max-w-full mt-4">
-					<Divider key={bank + '_' + index} word={bank} balance={formatAmount(bankTotals[bank])} />
+					<Divider>{bank}</Divider>
 					<div className="h-fit max-w-full mt-1 px-2 flex flex-nowrap overflow-x-scroll overflow-y-hidden">
 						{accounts
 							.filter((account) => account.parent === bank)
--- frontend/src/components/accounts/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import {Divider} from '@tremor/react';
-
-export default function BankDivider({word, balance}) {
-	return (
-		<Divider>
-			{word} &bull; £{balance}
-		</Divider>
-	);
-}
