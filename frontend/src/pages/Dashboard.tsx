5738ec5ee0c991294e794b67a0e47791f520a944
PocketCoder <jake.williams77@gmail.com>
2024-06-18 10:37:45 +0100

    feat: progress bar starts on £0 or earliest entry for that year

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -14,6 +14,7 @@ export default function Dashboard() {
 	const len = userData.netWorth[prefs.year].length;
 	const nw = userData.netWorth[prefs.year][len - 1].amount;
 	const goal = userData.prefs['goal'][prefs.year];
+	const start = userData.netWorth[prefs.year][0].amount | 0;
 	return (
 		<main className="p-6 min-h-full h-full w-full mb-16">
 			<h1 className="text-2xl">Dashboard</h1>
@@ -29,7 +30,7 @@ export default function Dashboard() {
 					</div>
 				</div>
 				<div className="flex justify-evenly items-center my-4 w-full">
-					<ProgressBar start={25000} goal={goal} curr={nw} />
+					<ProgressBar start={start} goal={goal} curr={nw} />
 					<NetWorthDonut data={userData.accounts} year={prefs.year} />
 				</div>
 				<div className='flex-col my-4 mx-auto w-10/12 h-auto"'>
