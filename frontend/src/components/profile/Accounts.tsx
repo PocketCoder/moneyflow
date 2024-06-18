c9956904171066e53b48e9613931f9cdec55434d
PocketCoder <jake.williams77@gmail.com>
2024-06-18 13:29:13 +0100

    UI: new layout for tags and tag switch (touchable vs untouchable)

--- frontend/src/components/profile/Accounts.tsx
+++ frontend/src/components/profile/Accounts.tsx
@@ -58,44 +58,68 @@ export default function Accounts() {
 					<></>
 				) : (
 					<Card key={`${a.name}_${i}`} className="my-2 mx-1 w-5/12 h-fit">
-						<div className="flex flex-row justify-between items-center">
+						<div className="flex flex-row justify-between items-start">
 							<div>
 								<Title>{a.name}</Title>
 								<Subtitle>{a.parent}</Subtitle>
 								<Text>{a.type}</Text>
 							</div>
-							<div>
-								<div className="flex items-center mt-2">
-									<TextInput
-										value={a.newTag || ''}
-										onChange={(e) => handleNewTagChange(i, e.target.value)}
-										placeholder="Add new tag"
-										className="mx-1"
-									/>
-									<Button
-										onClick={() => handleAddTag(i)}
-										size="xs"
-										className="p-0 mx-1 bg-transparent border-none hover:bg-blue-300">
-										<PlusCircleIcon className="text-black h-6" />
-									</Button>
-								</div>
+							<div className="flex flex-row">
+								<Text
+									className={clsx({
+										'text-black': a.tags.includes('untouchable'),
+										underline: a.tags.includes('untouchable')
+									})}>
+									Untouchable
+								</Text>
+								<Switch
+									className="px-2"
+									defaultChecked={a.tags.includes('touchable')}
+									onChange={(e) => handleSwitchChange(a)}
+								/>
+								<Text
+									className={clsx({
+										'text-black': a.tags.includes('touchable'),
+										underline: a.tags.includes('touchable')
+									})}>
+									Touchable
+								</Text>
 							</div>
 						</div>
-						<div className="w-full mt-5 flex flex-row justify-start items-center flex-wrap">
-							{a.tags.map((t, z) => (
-								<div key={`${t}_${z}`} className="flex items-center mr-4 my-1">
-									<>
-										<Badge>{t}</Badge>
-										<Button
-											onClick={() => handleRemoveTag(i, z)}
-											size="xs"
-											className="p-0 mx-1 bg-transparent border-none hover:bg-blue-300"
-											tooltip="Delete tag">
-											<MinusCircleIcon className="text-black h-6" />
-										</Button>
-									</>
-								</div>
-							))}
+						<div className="w-full mt-5 flex flex-row justify-between items-center">
+							<Text className="mr-2">Tags:</Text>
+							<div className="w-full flex flex-row justify-start items-center flex-nowrap overflow-y-scroll">
+								{a.tags.map((t, z) =>
+									t === 'touchable' || t === 'untouchable' ? <></> : (
+										<div
+											key={`${t}_${z}`}
+											className="flex justify-start items-center mr-1 p-1 border-solid border-2 rounded">
+											<Text className="font-mono text-xs">{t}</Text>
+											<Button
+												onClick={() => handleRemoveTag(i, z)}
+												size="xs"
+												className="p-0.5 ml-1 bg-red-200 border-none hover:bg-blue-300"
+												tooltip="Delete tag">
+												<XMarkIcon className="text-black h-3" />
+											</Button>
+										</div>
+									)
+								)}
+							</div>
+							<TextInput
+								value={a.newTag || ''}
+								onChange={(e) => handleNewTagChange(i, e.target.value)}
+								placeholder="Add new tag..."
+								className="mx-1"
+							/>
+							<Button
+								onClick={() => {
+									handleAddTag(i);
+								}}
+								size="xs"
+								className="p-0 mx-1 hover:bg-blue-600 border-none bg-transparent">
+								<PlusCircleIcon className="text-black h-6 hover:text-white" />
+							</Button>
 						</div>
 					</Card>
 				)
