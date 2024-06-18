8a62685cef04ae400bea56d204dabdcfd5b89587
PocketCoder <jake.williams77@gmail.com>
2024-06-18 09:30:26 +0100

    complete user record is now returned by the server

--- server/index.js
+++ server/index.js
@@ -25,12 +25,18 @@ app.get('/', (req, res) => {
 });
 app.get('/authID/:id', checkJwt, async (req, res) => {
     try {
-        const id = await prisma.users.findUnique({
+        const user = await prisma.users.findUnique({
             where: {
                 auth0id: req.params.id
+            },
+            select: {
+                id: true,
+                name: true,
+                auth0id: true,
+                preferences: true
             }
         });
-        res.status(200).json(id.id);
+        res.status(200).json(user);
     }
     catch (e) {
         console.error(`Error in /authID/${req.params.id}`);
