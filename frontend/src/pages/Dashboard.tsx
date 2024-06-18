7075affe80500092ddb1dc86348317443ec40098
PocketCoder <jake.williams77@gmail.com>
2024-06-18 10:33:18 +0100

    feat: shows goal according to user preferences goal

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -10,9 +10,10 @@ import {Title, Metric} from '@tremor/react';
 
 export default function Dashboard() {
 	const {userData} = useContext(UserContext);
-	const {preferences} = useContext(PrefContext);
-	const len = userData.netWorth[preferences.year].length;
-	const nw = userData.netWorth[preferences.year][len - 1].amount;
+	const {preferences: prefs} = useContext(PrefContext);
+	const len = userData.netWorth[prefs.year].length;
+	const nw = userData.netWorth[prefs.year][len - 1].amount;
+	const goal = userData.prefs['goal'][prefs.year];
 	return (
 		<main className="p-6 min-h-full h-full w-full mb-16">
 			<h1 className="text-2xl">Dashboard</h1>
@@ -28,8 +29,8 @@ export default function Dashboard() {
 					</div>
 				</div>
 				<div className="flex justify-evenly items-center my-4 w-full">
-					<ProgressBar start={25000} goal={40000} curr={nw} />
-					<NetWorthDonut data={userData.accounts} year={preferences.year} />
+					<ProgressBar start={25000} goal={goal} curr={nw} />
+					<NetWorthDonut data={userData.accounts} year={prefs.year} />
 				</div>
 				<div className='flex-col my-4 mx-auto w-10/12 h-auto"'>
 					<NetWorthChart />
