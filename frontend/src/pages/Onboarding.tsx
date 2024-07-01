d656e16f6972afa3a69d651286b474547f10ba47
PocketCoder <jake.williams77@gmail.com>
2024-07-01 10:02:07 +0100

    Set up onboarding page & structure
    1. Sets blank netWorth, Goal, and Accounts variables / states.
    2. Array of step names for ticker.
    3. Navigate function for returning to the dashboard, forcing a refresh so data is pulled (FIX: store new data in state context.)
    4. Progress Circle UI cleanup with step names from array
    5. Route Paths with their required props
    6. Buttons with disabled, and text requirements based on variables

--- frontend/src/pages/Onboarding.tsx
+++ frontend/src/pages/Onboarding.tsx
@@ -1,5 +1,6 @@
+import {useState} from 'react';
 import {ProgressCircle, Button} from '@tremor/react';
-import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
+import {Routes, Route, useLocation, useNavigate, redirect} from 'react-router-dom';
 import {ChevronLeftIcon, ChevronRightIcon} from '@heroicons/react/24/outline';
 
 import Welcome from './onboarding/Welcome';
@@ -9,6 +10,17 @@ import Goal from './onboarding/Goal';
 import Finish from './onboarding/Finish';
 
 export default function Onbording() {
+	const [netWorth, setNetWorth] = useState(0);
+	const [goal, setGoal] = useState(0);
+	const [accounts, setAccounts] = useState([
+		{
+			name: '',
+			parent: '',
+			type: '',
+			balance: 0
+		}
+	]);
+
 	const location = useLocation();
 	const navigate = useNavigate();
 	const steps = [
@@ -18,6 +30,7 @@ export default function Onbording() {
 		'/onboarding/goal',
 		'/onboarding/finish'
 	];
+	const stepNames = ['Welcome', 'Getting set up', 'Calculate Net Worth', 'Setting a goal', 'Finished!'];
 	const currentStep = steps.indexOf(location.pathname);
 	const curr = currentStep + 1;
 	const end = steps.length;
@@ -34,39 +47,55 @@ export default function Onbording() {
 		}
 	}
 
+	function navigateAndRefresh() {
+		const baseUrl = window.location.origin;
+		const dashboardUrl = `${baseUrl}/dashboard`;
+		window.location.href = dashboardUrl;
+	  };
+
 	return (
 		<main className="p-6 min-h-full h-full w-full mb-16">
 			<div className="flex items-center justify-between w-full h-fit">
 				<h1 className="text-2xl">Onboarding</h1>
-				<ProgressCircle value={(curr / end) * 100} size="md">
-					<span>
-						{curr} / {end}
-					</span>
-				</ProgressCircle>
+				<div className="flex flex-row items-center ">
+					<span className="mr-6 font-bold">{stepNames[currentStep]}</span>
+					<ProgressCircle value={(curr / end) * 100} size="md">
+						<span>
+							{curr} / {end}
+						</span>
+					</ProgressCircle>
+				</div>
 			</div>
 			<Routes>
-                <Route path="*" element={<Welcome nextStep={nextStep} prevStep={prevStep} />} />
-				<Route path="welcome" element={<Welcome nextStep={nextStep} prevStep={prevStep} />} />
-				<Route path="setup" element={<Setup nextStep={nextStep} prevStep={prevStep} />} />
-				<Route path="networth" element={<NetWorth nextStep={nextStep} prevStep={prevStep} />} />
-				<Route path="goal" element={<Goal nextStep={nextStep} prevStep={prevStep} />} />
-				<Route path="finish" element={<Finish nextStep={nextStep} prevStep={prevStep} />} />
+				<Route path="*" element={<Welcome />} />
+				<Route path="welcome" element={<Welcome />} />
+				<Route path="setup" element={<Setup accounts={accounts} setAccounts={setAccounts} setNW={setNetWorth} />} />
+				<Route path="networth" element={<NetWorth accounts={accounts} nw={netWorth} />} />
+				<Route path="goal" element={<Goal nw={netWorth} goal={goal} setGoal={setGoal} />} />
+				<Route path="finish" element={<Finish accounts={accounts} goal={goal} netWorth={netWorth} />} />
 			</Routes>
 			<div className="fixed left-8 bottom-20 w-28 flex justify-between items-center">
 				{currentStep < 1 ? (
-					<></>
+					<Button className="p-2 bg-transparent border-none" disabled={true}>
+						<ChevronLeftIcon className="h-6 text-black" />
+					</Button>
 				) : (
 					<Button onClick={prevStep} className="p-2 bg-transparent border-none">
-						<ChevronLeftIcon className="h-6 text-black" />
+						<ChevronLeftIcon className="h-6 text-black hover:text-white" />
 					</Button>
 				)}
 				{currentStep + 1 === end ? (
-					<Button onClick={() => {navigate('/dashboard')}}>
-                        Finish
-                    </Button>
+					<Button
+						onClick={navigateAndRefresh}>
+						Finish
+					</Button>
 				) : (
-					<Button onClick={nextStep} className="p-2 bg-transparent border-none">
-						<ChevronRightIcon className="h-6 text-black" />
+					<Button
+						onClick={nextStep}
+						className="p-2 bg-transparent border-none text-black hover:text-white"
+						icon={ChevronRightIcon}
+						iconPosition="right">
+						{currentStep <= 0 ? 'Start' : 'Next'}
 					</Button>
 				)}
 			</div>
