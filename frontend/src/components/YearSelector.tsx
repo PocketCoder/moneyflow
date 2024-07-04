d1d8fb6f7bd0356ca73632387eb42637aace6d35
PocketCoder <jake.williams77@gmail.com>
2024-07-04 09:36:39 +0100

    fix: current year wasn't highlighting on first load
    due to a mismatch of types (=== vs ==)

--- frontend/src/components/YearSelector.tsx
+++ frontend/src/components/YearSelector.tsx
@@ -2,7 +2,6 @@ import {Card, Button} from '@tremor/react';
 
 export default function YearSelector({years, preferences, setPreferences}) {
 	const sortedYears = [...years].sort((a, b) => b - a);
-
 	return (
 		<Card className="h-fit max-h-fit py-4 px-1 min-w-fit max-w-max fixed bottom-20 left-1/2 transform -translate-x-1/2 flex justify-evenly items-center">
 			{sortedYears.length === 0 ? (
@@ -14,7 +13,7 @@ export default function YearSelector({years, preferences, setPreferences}) {
 					<Button
 						key={year}
 						size="xs"
-						variant={preferences.year === year ? 'primary' : 'secondary'}
+						variant={preferences.year == year ? 'primary' : 'secondary'}
 						className="mx-5"
 						onClick={() => setPreferences({year})}>
 						{year}
