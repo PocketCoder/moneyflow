f52666ebb9c52d32ae703461d99af3af253dedce
PocketCoder <jake.williams77@gmail.com>
2025-01-12 11:47:39 +0000

    Bank Logos

---
+++ src/lib/bankLogos.ts
@@ -0,0 +1,18 @@
+export const bankLogos: Record<string, string> = {
+	'GOV.UK': '/bank-logos/gov_uk.png',
+	'GOV.UK_ICON': '/bank-logos/gov_uk_small.svg',
+	'UNIVERSAL CREDIT': '/bank-logos/gov_uk.png',
+	LLOYDS: '/bank-logos/lloyds.svg',
+	PLUM: '/bank-logos/plum.svg',
+	'SCOTTISH WIDOWS': '/bank-logos/scottish_widows.svg',
+	YBS: '/bank-logos/ybs.svg',
+	CHIP: '/bank-logos/chip.svg',
+	CHASE: '/bank-logos/chase.svg',
+	MONEYBOX: '/bank-logos/moneybox.svg',
+	VANGUARD: '/bank-logos/vanguard.svg',
+	MONZO: '/bank-logos/monzo.png',
+	BARCLAYS_SMALL: '/bank-logos/barclays_small.svg',
+	BARCLAYS: '/bank-logos/barclays.svg',
+	SANTANDER: '/bank-logos/santander.svg',
+	MONZO_SMALL: '/bank-logos/monzo_small.png'
+};
