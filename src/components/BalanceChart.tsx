130ded726c1f54cac4d5e7be9bc2fc8d8dd3ac08
PocketCoder <jake.williams77@gmail.com>
2025-01-15 08:29:02 +0000

    Chart cosmetic changes

--- src/components/BalanceChart.tsx
+++ src/components/BalanceChart.tsx
@@ -2,6 +2,7 @@
 
 import {AccountData, BalanceData} from '@/lib/types';
 import {ResponsiveContainer, AreaChart, Area, XAxis, YAxis, Tooltip} from 'recharts';
+import {currencyFormatter} from '@/lib/utils';
 
 export default function BalanceChart({data, type}: {data: BalanceData[]; type: Partial<AccountData>['type']}) {
 	const colour = type === 'Debt' ? '#b91c1c' : '#14b8a6';
@@ -9,9 +10,9 @@ export default function BalanceChart({data, type}: {data: BalanceData[]; type: P
 		<ResponsiveContainer width={'100%'} height={'100%'}>
 			<AreaChart data={data} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
 				<Area type="monotone" dataKey="amount" fill={colour} stroke={colour} />
-				<XAxis dataKey="date" />
-				<YAxis />
-				<Tooltip />
+				<XAxis dataKey="date" interval={'equidistantPreserveStart'} />
+				<YAxis tickFormatter={currencyFormatter} width={70} />
+				<Tooltip formatter={currencyFormatter} />
 			</AreaChart>
 		</ResponsiveContainer>
 	);
