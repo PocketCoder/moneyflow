2b689d07ccbe74ac557ad1a8a7f7212bc26e0bf4
PocketCoder <jake.williams77@gmail.com>
2025-01-15 10:07:34 +0000

    Avoid lint errors

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -8,7 +8,7 @@ export default async function Settings() {
 	const userDataResult = await sql`SELECT name, preferences FROM users WHERE id=${process.env.USERID}`;
 	const userData: UserData = userDataResult.rows[0] as UserData;
 	const year = new Date().getFullYear();
-	// TODO: Make save goals work.
+	// TODO: Redo Goals.
 	return (
 		<main className="flex flex-col gap-4">
 			<Card className="flex justify-between items-center">
@@ -20,23 +20,6 @@ export default async function Settings() {
 			</Card>
 			<Card className="flex flex-col justify-between items-between gap-2">
 				<h2 className="font-bold text-lg">Goal</h2>
-				{Object.keys(userData.preferences.goal).map((key: string, i: number) => (
-					<div key={i} className="flex items-center justify-between">
-						<h2 className="font-bold">{key}</h2>
-						<div className="flex w-90 gap-4">
-							<Input
-								type="number"
-								placeholder="Goal"
-								className="w-80"
-								defaultValue={userData.preferences.goal[key]}
-								disabled={year.toString() !== key}
-							/>
-							<Button variant="primary" disabled={year.toString() !== key}>
-								Save
-							</Button>
-						</div>
-					</div>
-				))}
 			</Card>
 		</main>
 	);
