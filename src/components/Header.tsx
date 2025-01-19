d8ee96d659c2ab5f03993f1bb1c43a8b3c74ebb3
PocketCoder <jake.williams77@gmail.com>
2025-01-19 09:42:34 +0000

    Add logo to header for fun
    Plus change title colour to white

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import {getSession} from '@auth0/nextjs-auth0';
 import {UserProfile} from '@auth0/nextjs-auth0/client';
 import {Button} from '@/components/Tremor/Button';
@@ -8,7 +9,10 @@ export default async function Header() {
 	const user: UserProfile | undefined = session?.user;
 	return (
 		<header className="fixed top-0 z-50 flex h-14 w-screen items-center justify-between bg-teal-500 px-6">
-			<h2 className="text-2xl font-bold">Moneyflow</h2>
+			<div className="flex gap-4">
+				<Image src="/logo.png" alt="logo" width={32} height={32} />
+				<h2 className="text-2xl font-bold text-white">Moneyflow</h2>
+			</div>
 			<div className="flex gap-4">
 				{user ? (
 					<>
