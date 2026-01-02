7a163e5f2117371e8afe325fdf6f83fbfbacc86d
jake <jake.williams77@gmail.com>
2026-01-02 22:51:39 +0000

    chore: remove leftover email-related code

--- src/components/auth/signin-form.tsx
+++ src/components/auth/signin-form.tsx
@@ -1,5 +1,4 @@
 import {signIn} from '@/auth';
-import {Input} from '../ui/input';
 import {Button} from '../ui/button';
 import {GithubIcon} from '../ui/icons/lucide-github';
 
@@ -7,22 +6,14 @@ export function SignIn() {
 	return (
 		<div className="flex flex-col gap-4">
 			<form
-				action={async (formData) => {
-					'use server';
-					await signIn('mailgun', formData);
-				}}
-				className="flex gap-4">
-				<Input type="text" name="email" placeholder="Email" />
-				<Button type="submit">Sign In with Email</Button>
-			</form>
-			<Button
-				variant={'outline'}
-				onClick={async () => {
+				action={async () => {
 					'use server';
 					await signIn('github');
 				}}>
-				Sign In with <GithubIcon className="mr-2 h-4 w-4" />
-			</Button>
+				<Button variant={'outline'} type="submit">
+					Sign In with <GithubIcon className="mr-2 h-4 w-4" />
+				</Button>
+			</form>
 		</div>
 	);
 }
