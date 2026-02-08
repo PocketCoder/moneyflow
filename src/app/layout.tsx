9e65546692618b01b899beb2d52bddbcb133bbb4
jake <jake.williams77@gmail.com>
2026-02-08 10:29:34 +0000

    fix: HMR & styling for components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import {SpeedInsights} from '@vercel/speed-insights/next';
 import {Analytics} from '@vercel/analytics/next';
 import {Comfortaa} from 'next/font/google';
-import './globals.css';
+import '../globals.css';
 
 import {Toaster} from '@/components/ui/sonner';
 import NavBar from '@/components/NavBar';
