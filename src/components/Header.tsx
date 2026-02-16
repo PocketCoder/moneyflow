a2f171838e59f9dbc2d47dadb0e952b5e525772b
jake <jake.williams77@gmail.com>
2026-02-16 17:16:26 +0000

    UI: new name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
 		<header className="bg-primary text-primary-foreground fixed top-0 z-50 flex h-14 w-screen items-center justify-between px-6 shadow-sm">
 			<div className="flex gap-4">
 				<Image src="/logo.svg" alt="logo" width={32} height={32} />
-				<h2 className="text-primary-foreground text-2xl font-bold">Moneyflow</h2>
+				<h2 className="text-primary-foreground text-2xl font-bold">Vantage</h2>
 			</div>
 			<div className="flex gap-4">
 				{user ? (
